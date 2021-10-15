f68701e35958a74170f23f3311225ff6dd7814b1
sunchanglong <13674371024@163.com>
2021-10-15 18:47:53 +0800

    4.sink

--- sinks.go
+++ sinks.go
@@ -60,6 +60,8 @@ func setSinkView(s Sink, v ByteView) error {
 	// a ByteView. This is a fast path to minimize copies when the
 	// item was already cached locally in memory (where it's
 	// cached as a ByteView)
+	// 一个 viewsetter 是一个实现了sink 接口的类型，所以可以接收到一个 byteview 值
+	// 当一个条目缓存在内存中时，这种方式的复制量最少
 	type viewSetter interface {
 		setView(v ByteView) error
 	}
@@ -117,6 +119,7 @@ func (s *stringSink) SetProto(m proto.Message) error {
 }
 
 // ByteViewSink returns a Sink that populates a ByteView.
+// 根据参数dst构造 byteViewSink 结构体实例
 func ByteViewSink(dst *ByteView) Sink {
 	if dst == nil {
 		panic("nil dst")
@@ -124,6 +127,7 @@ func ByteViewSink(dst *ByteView) Sink {
 	return &byteViewSink{dst: dst}
 }
 
+// 属性dst为一个ByteView指针
 type byteViewSink struct {
 	dst *ByteView
 
@@ -135,6 +139,9 @@ type byteViewSink struct {
 	// really care about at least once (in a handler), but if
 	// multiple handlers fail (or multiple functions in a program
 	// using a Sink), it's okay to re-use the same one.
+	// 如果此代码最终跟踪到至少调用了一个 set* 方法，请不要将多次调用 set 方法视为错误。
+	// Lorry 的 payload.go 做到了这一点，这是有道理的。 此文件顶部关于“正好是 Set 方法之一”的注释过于严格。
+	// 我们真正关心至少一次（在处理程序中），但是如果多个处理程序失败（或使用 Sink 的程序中的多个函数），可以重用相同的一个
 }
 
 func (s *byteViewSink) setView(v ByteView) error {
@@ -146,6 +153,7 @@ func (s *byteViewSink) view() (ByteView, error) {
 	return *s.dst, nil
 }
 
+// 设置 byteViewSink 中 ByteView 的 b
 func (s *byteViewSink) SetProto(m proto.Message) error {
 	b, err := proto.Marshal(m)
 	if err != nil {
@@ -155,17 +163,20 @@ func (s *byteViewSink) SetProto(m proto.Message) error {
 	return nil
 }
 
+// 复制b，初始化 byteViewSink 的 dst
 func (s *byteViewSink) SetBytes(b []byte) error {
 	*s.dst = ByteView{b: cloneBytes(b)}
 	return nil
 }
 
+// 通过使用string类型的v初始化一个 ByteView 后初始化 byteViewSink 的 dst
 func (s *byteViewSink) SetString(v string) error {
 	*s.dst = ByteView{s: v}
 	return nil
 }
 
 // ProtoSink returns a sink that unmarshals binary proto values into m.
+// 使用proto.Message类型的m初始化protoSink的dst
 func ProtoSink(m proto.Message) Sink {
 	return &protoSink{
 		dst: m,
@@ -179,10 +190,12 @@ type protoSink struct {
 	v ByteView // encoded
 }
 
+// 返回protoSink的ByteView类型的v
 func (s *protoSink) view() (ByteView, error) {
 	return s.v, nil
 }
 
+// 将s.dst反序列化后丢给b，并且复制一份丢给 protoSink 中 ByteView 的 b
 func (s *protoSink) SetBytes(b []byte) error {
 	err := proto.Unmarshal(b, s.dst)
 	if err != nil {
@@ -193,6 +206,7 @@ func (s *protoSink) SetBytes(b []byte) error {
 	return nil
 }
 
+// 将b解码后写入s.dst
 func (s *protoSink) SetString(v string) error {
 	b := []byte(v)
 	err := proto.Unmarshal(b, s.dst)
@@ -204,6 +218,7 @@ func (s *protoSink) SetString(v string) error {
 	return nil
 }
 
+// 将m写入 protoSink 的 dst
 func (s *protoSink) SetProto(m proto.Message) error {
 	b, err := proto.Marshal(m)
 	if err != nil {
@@ -225,10 +240,12 @@ func (s *protoSink) SetProto(m proto.Message) error {
 // AllocatingByteSliceSink returns a Sink that allocates
 // a byte slice to hold the received value and assigns
 // it to *dst. The memory is not retained by groupcache.
+// 分配一个字节切片来保存接收到的数据
 func AllocatingByteSliceSink(dst *[]byte) Sink {
 	return &allocBytesSink{dst: dst}
 }
 
+// 有一个字节切片指针类型的属性dst
 type allocBytesSink struct {
 	dst *[]byte
 	v   ByteView
@@ -238,6 +255,7 @@ func (s *allocBytesSink) view() (ByteView, error) {
 	return s.v, nil
 }
 
+// 设置allocBytesSink的v，同时复制v中的b或者s丢给dst
 func (s *allocBytesSink) setView(v ByteView) error {
 	if v.b != nil {
 		*s.dst = cloneBytes(v.b)
@@ -248,6 +266,7 @@ func (s *allocBytesSink) setView(v ByteView) error {
 	return nil
 }
 
+// 这个得从下面的setBytesOwned开始往上看
 func (s *allocBytesSink) SetProto(m proto.Message) error {
 	b, err := proto.Marshal(m)
 	if err != nil {
@@ -256,10 +275,12 @@ func (s *allocBytesSink) SetProto(m proto.Message) error {
 	return s.setBytesOwned(b)
 }
 
+// 复制一份b，然后调用setBytesOwned
 func (s *allocBytesSink) SetBytes(b []byte) error {
 	return s.setBytesOwned(cloneBytes(b))
 }
 
+// 使用b设置allocBytesSink的dst和ByteView
 func (s *allocBytesSink) setBytesOwned(b []byte) error {
 	if s.dst == nil {
 		return errors.New("nil AllocatingByteSliceSink *[]byte dst")
@@ -270,6 +291,7 @@ func (s *allocBytesSink) setBytesOwned(b []byte) error {
 	return nil
 }
 
+// 字符串转成[]byte后进行和上面类似的操作
 func (s *allocBytesSink) SetString(v string) error {
 	if s.dst == nil {
 		return errors.New("nil AllocatingByteSliceSink *[]byte dst")
@@ -284,6 +306,7 @@ func (s *allocBytesSink) SetString(v string) error {
 // bytes to *dst. If more bytes are available, they're silently
 // truncated. If fewer bytes are available than len(*dst), *dst
 // is shrunk to fit the number of bytes available.
+// 截面字节切片Sink，写入len(*dst)个字节到dst，如果长度超了会被截断，少了则dst会收缩长度来适配
 func TruncatingByteSliceSink(dst *[]byte) Sink {
 	return &truncBytesSink{dst: dst}
 }
@@ -297,6 +320,7 @@ func (s *truncBytesSink) view() (ByteView, error) {
 	return s.v, nil
 }
 
+// 从下面的setBytesOwned开始看
 func (s *truncBytesSink) SetProto(m proto.Message) error {
 	b, err := proto.Marshal(m)
 	if err != nil {
